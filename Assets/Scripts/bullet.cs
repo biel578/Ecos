0015cc845b7acb613cb615035ac32fa323f14974
biel578 <bielaraujo578@gmail.com>
2024-11-04 05:13:25 -0300

    Commit Auxiliar Final

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class bullet : MonoBehaviour
 {
+    private int vidas = 20;
     public GameObject hitEffect;
 
     void Start()
@@ -17,6 +19,16 @@ public class bullet : MonoBehaviour
             GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
             Destroy(effect, 0.55f);
             Destroy(gameObject);
+        }
+        if(collision.gameObject.CompareTag("perna"))
+        {
+            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 0.55f);
+            for(int i = 0; i<=20;)
+            {
+                Destroy(collision.gameObject);
+                i++;
+            }
         }
 
         if (collision.gameObject.CompareTag("protetor"))
