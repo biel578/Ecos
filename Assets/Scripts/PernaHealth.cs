05d960b6a95583bb49dfd381d137efde3640352a
Sofia1653 <sofedu2005@gmail.com>
2024-11-28 17:57:20 -0300

    tentando mudar as vidas da perna

--- Assets/Scripts/PernaHealth.cs
+++ Assets/Scripts/PernaHealth.cs
@@ -3,44 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class PernaHealth : MonoBehaviour
 {
     public int currentHealth;
     public int maxHealth = 20;
+    private bool hasTakenDamage = false;
+
     void Start()
     {
         currentHealth = maxHealth;
     }
+
     public void ChangeHealth(int amount)
     {
         currentHealth += amount;
-        if(currentHealth < 0)
-        {
-            gameObject.SetActive(false);
-        }
-    }
-    void TakeDamage(int amount)
-    {
-        currentHealth -= amount;
 
-        if(currentHealth <=0 )
+        if (currentHealth <= 0)
         {
             Destroy(gameObject);
         }
     }
+
     private void OnTriggerStay2D(Collider2D collider)
     {
-        if (collider.tag != "lama")
+        if (collider.tag != "lama" && !hasTakenDamage)
         {
-            gameObject.transform.parent = collider.gameObject.transform;
-            Destroy(gameObject);
             GetComponent<CircleCollider2D>().enabled = false;
+
+            hasTakenDamage = true;
+
+            ChangeHealth(-5);
         }
     }
+
     private IEnumerator LoadSceneAfterDelay(float delay)
     {
         yield return new WaitForSeconds(delay);
         SceneManager.LoadScene("Game Over Tela");
     }
 }
+
+
